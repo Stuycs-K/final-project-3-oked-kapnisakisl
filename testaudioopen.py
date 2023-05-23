c2a10ab31c2d5f9aae1d27ddc6eb7bf0e3ef2c0d
Leftykap <Leftykap@outlook.com>
2023-05-23 10:10:02 -0400

    tried writing last bit, seems like would work but takes FOREVER because its a string??? and python? idk, probably going to switch to what David is trying with bitwise operators.

--- testaudioopen.py
+++ testaudioopen.py
@@ -8,4 +8,19 @@ datahex = data.hex()
 #print(data)
 
 test = BitArray(hex=datahex)
+#print(test.bin)
+print(len(test.bin))
+
+#Testing writing an encode, encountering some issues with the fact that we cant
+#figure out header length but going for anyway assuming 46 bytes.
+
+i = (46 * 8) #magic nums for length of header
+
+while(i < len(test.bin)):
+    test[i] = True
+    i+=8
+    if(i % (8*500) == 0):
+        print(i / len(test.bin))
+
+print("===================")
 print(test.bin)
