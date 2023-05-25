280ae91e48801a43b0d4c062e20822f242458a37
Leftykap <Leftykap@outlook.com>
2023-05-25 10:31:34 -0400

    wrote encoding into data, no way to check if it works though but code does run!

--- testaudioopen.py
+++ testaudioopen.py
@@ -1,7 +1,11 @@
 # from bitstring import BitArray
 # import sys
 
-message = "hi!"
+###GLOBAL VARIABLES!!!
+HEADER_LEN = 50
+
+#Mr k suggestion demonstrate audio quality difference from 1->2 -> 3 -> 4 bytes of payload
+message = "hzi!"
 messagebytes = bytearray(message,'utf-8')
 with open('Bruh_Sound_Effect.wav', 'rb') as f:
     data = f.read()
@@ -9,21 +13,32 @@ with open('Bruh_Sound_Effect.wav', 'rb') as f:
 print(data[3])
 print(bin(data[3]))
 
-dataray = bytearray(data)
-print(dataray[3])
+data = bytearray(data)
+print("dataray3",data[3])
 #dataray[3] = 248
 #print("testing!!")
 #print(dataray[3])
 #print(bin(dataray[3]))
 #this shows the bytes are 8 bits (duh) but it doesnt print leading 0s.
-
 #print(data)
 outdata = data
-for i in range (50,len(data)):
+for i in range (HEADER_LEN,len(data)):
     #i represents the byte we are on.
     byte = data[i]
-    outdata = (data[i] & 248)#I think 248 is right? all but last 3 on
-    for b in range (0,8):# 0-7, not sure if this will work?
+    outdata[i] = (data[i] & 248)#I think 248 is right? all but last 3 on
+ticker = HEADER_LEN
+for x in message:
+    #print("bin x:",bin(ord(x)))
+    check = (ord(x) & (64 + 128))
+    #print("preshift:",check)
+    check = (check >> 6) # Right now, we are reading first 2 bits (bits on left.) so looks like 11000000. Move all the way to the right, so aligned for write
+    #print("postshift:",check)
+    outdata[ticker] = (outdata[ticker] ^ check)#write now correctly shifted data into outdata
+    ticker += 1
+    if(ticker >= len(data)):
+        print("RAN OUT OF ENCODING SPACE!!!!!!!!!!!")
+    #Go through every 2 bits, writing, then shifting left(?) 2
+
         #data[i]
 #We need to figure out encoding scheme here.
     #outdata = data[i]
@@ -54,4 +69,3 @@ for i in range (50,len(data)):
 
 #print("===================")
 #print(test.bin)
-
