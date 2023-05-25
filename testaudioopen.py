a69b42c2d279f914fc79df718f7a1b9b52982118
Leftykap <Leftykap@outlook.com>
2023-05-25 10:52:29 -0400

    removed useless commented stuff, probably commiting too often but oh well

--- testaudioopen.py
+++ testaudioopen.py
@@ -49,30 +49,3 @@ for i in range(ticker,len(data)):
     data[i] = (data[i] ^ 4)
 #for x in range( HEADER_LEN,len(data)):
 #    print("x:",x,"bin:",bin(outdata[x]))
-
-
-
-#And it with all but last bit on, then or it with what we want. And should clear, or should add.
-
-
-# datastr = bin(int.from_bytes(data, byteorder=sys.byteorder))
-
-# print(datastr)
-
-#test = BitArray(hex=datahex)
-#print(test.bin)
-#print(len(test.bin))
-
-#Testing writing an encode, encountering some issues with the fact that we cant
-#figure out header length but going for anyway assuming 46 bytes.
-
-#i = (46 * 8) #magic nums for length of header
-
-#while(i < len(test.bin)):
-#    test[i] = True
-#    i+=8
- #   if(i % (8*500) == 0):
-#        print(i / len(test.bin))
-
-#print("===================")
-#print(test.bin)
